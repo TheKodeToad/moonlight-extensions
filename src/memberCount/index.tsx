1a4238b775dd5328f5d269fe868b7be66b16d1a4
TheKodeToad <TheKodeToad@proton.me>
2025-01-20 00:41:41 +0000

    Initial commit (memberCount)

---
+++ src/memberCount/index.tsx
@@ -0,0 +1,18 @@
+import { ExtensionWebExports } from "@moonlight-mod/types";
+
+export const patches: ExtensionWebExports["patches"] = [
+	{
+		find: '"GuildHeader"',
+		replace: {
+			match: /(?<=variant:"text-md\/semibold",lineClamp:1,className:\i\.name,children:)\i.toString\(\)/,
+			replacement: "require('memberCount_components').wrapGuildNameComponent(arguments[0].guild,$&)"
+		}
+	}
+];
+
+// https://moonlight-mod.github.io/ext-dev/webpack/#webpack-module-insertion
+export const webpackModules: ExtensionWebExports["webpackModules"] = {
+	components: {
+		dependencies: ["react", "stores", "spacepack_spacepack"]
+	}
+};
---
+++ src/memberCount/webpackModules/components.tsx
@@ -0,0 +1,49 @@
+import { GuildMemberCountStore } from "@moonlight-mod/wp/common_stores";
+import { useStateFromStores } from "@moonlight-mod/wp/discord/packages/flux";
+import React from "@moonlight-mod/wp/react";
+import spacepack from "@moonlight-mod/wp/spacepack_spacepack";
+
+const { statusOnline, statusOffline, statusWrapper, count } = spacepack.findByCode(
+	"inviteSplash:",
+	"statusOnline:",
+	"statusOffline:"
+)[0].exports as Record<string, string>;
+const locale: { getLocale(): string } = spacepack.findByCode("parse(document.cookie).locale")[0].exports.Z;
+
+export function wrapGuildNameComponent(guild, children: React.ReactNode) {
+	return <GuildNameWrapper guild={guild}>{children}</GuildNameWrapper>;
+}
+
+function GuildNameWrapper({ guild, children }: { guild; children: React.ReactNode }) {
+	return (
+		<div>
+			{children}
+			<MemberCount guild={guild} />
+		</div>
+	);
+}
+
+function MemberCount({ guild }: { guild }) {
+	const [onlineCount, totalCount] = useStateFromStores([GuildMemberCountStore], () => [
+		GuildMemberCountStore.getOnlineCount(guild.id) as number,
+		GuildMemberCountStore.getMemberCount(guild.id) as number
+	]);
+
+	if (onlineCount === undefined || totalCount === undefined) return undefined;
+
+	// TODO: I hope this works for all languages...
+	const numberFormat = new Intl.NumberFormat(locale.getLocale());
+
+	return (
+		<div className="memberCount-container">
+			<div className={statusWrapper}>
+				<i className={statusOnline}></i>
+				<span className={count}>{numberFormat.format(onlineCount)}</span>
+			</div>
+			<span className={statusWrapper}>
+				<i className={statusOffline}></i>
+				<span className={count}>{numberFormat.format(totalCount)}</span>
+			</span>
+		</div>
+	);
+}
