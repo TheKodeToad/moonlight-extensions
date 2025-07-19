e10afe5c3cab6f07c577ab6ba11a8cfe5ac9fd18
TheKodeToad <TheKodeToad@proton.me>
2025-07-19 15:43:18 +0100

    Fix for the latest Discord changes (again again)

--- src/memberCount/index.ts
+++ src/memberCount/index.ts
@@ -2,9 +2,9 @@ import { ExtensionWebExports } from "@moonlight-mod/types";
 
 export const patches: ExtensionWebExports["patches"] = [
 	{
-		find: '"GuildHeader"',
+		find: '"guild_header"',
 		replace: {
-			match: /\(0,\i\.jsx\)\(\i\.\i,{variant:"text-md\/semibold",lineClamp:1,className:\i\.name,children:\i\.toString\(\)\}\)/,
+			match: /\(0,\i\.jsx\)\(\i\.\i,{variant:"text-md\/semibold",lineClamp:1,className:\i\.name,children:\i\.name\}\)/,
 			replacement: "(require('memberCount_components').wrapGuildNameComponent(arguments[0].guild,$&))"
 		}
 	}
